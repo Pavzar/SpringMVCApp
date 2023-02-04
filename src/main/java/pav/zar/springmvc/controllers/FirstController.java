09e44d0866c7928ebe6599ed4a00347990769a55
Pavel <pavelzarov@yahoo.com>
2023-02-04 23:04:02 +0200

    exercise passing data from controller to view using model

--- src/main/java/pav/zar/springmvc/controllers/FirstController.java
+++ src/main/java/pav/zar/springmvc/controllers/FirstController.java
@@ -1,6 +1,7 @@
 package pav.zar.springmvc.controllers;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -11,9 +12,39 @@ public class FirstController {
 
     @GetMapping("/hello")
     public String helloPage(@RequestParam(value = "name", required = false) String name,
-                            @RequestParam(value = "surname", required = false) String surname) {
-        System.out.println(name + " " + surname);
+                            @RequestParam(value = "surname", required = false) String surname, Model model) {
+        //System.out.println(name + " " + surname);
+
+        model.addAttribute("message", "Hello, " + name + " " + surname);
+
         return "first/hello";
+    }
+
+    @GetMapping("/calculator")
+    public String calculate(@RequestParam("a") int a, @RequestParam("b") int b,
+                            @RequestParam("action") String action, Model model) {
+        double result = 0;
+
+        switch (action) {
+            case "multiplication":
+                result = a * b;
+                break;
+            case "addition":
+                result = a + b;
+                break;
+            case "subtraction":
+                result = a - b;
+                break;
+            case "division":
+                if (b != 0) {
+                    result = a / (double) b;
+                    break;
+                }
+        }
+
+        model.addAttribute("result", "Result is: " + result);
+
+        return "first/calculate";
     }
 
     @GetMapping("/goodbye")
